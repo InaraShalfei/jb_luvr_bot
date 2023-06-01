f2db0e922fecbaa211464632d641ad4032a16873
InaraShalfei <inarashugaikina@gmail.com>
2023-06-01 14:34:40 +0600

    Fix logic in notifications to calculate shift end and shift start datetime

--- luvr_bot/telegram_bot/notifications.py
+++ luvr_bot/telegram_bot/notifications.py
@@ -17,7 +17,7 @@ def send_shifts_start_soon_reminder():
     job_requests = JobRequest.objects.filter(Q(date_start__lte=today) & Q(date_end__gte=today) &
                                              (Q(last_notified_date__lt=today) | Q(last_notified_date__isnull=True)))
     for job_request in job_requests:
-        shift_start = datetime.datetime.combine(job_request.date_start, job_request.shift_time_start)
+        shift_start = datetime.datetime.combine(today, job_request.shift_time_start)
         if (shift_start - datetime.datetime.now()).total_seconds() <= 30 * 60:
             for assignment in job_request.assignments.all():
                 try:
@@ -39,9 +39,10 @@ def send_shifts_start_soon_reminder():
 
 def send_shifts_start_15_min_ago_reminder():
     today = datetime.datetime.today()
+    # TODO filter by last_notification_date = today
     job_requests = JobRequest.objects.filter(Q(date_start__lte=today) & Q(date_end__gte=today) & Q(last_notification_status=constants.STATUS_30_MIN_REM_SHIFT_START))
     for job_request in job_requests:
-        shift_start = datetime.datetime.combine(job_request.date_start, job_request.shift_time_start)
+        shift_start = datetime.datetime.combine(today, job_request.shift_time_start)
         if (datetime.datetime.now() - shift_start).total_seconds() >= 15 * 60:
             for assignment in job_request.assignments.all():
                 try:
@@ -62,10 +63,11 @@ def send_shifts_start_15_min_ago_reminder():
 
 def send_shifts_end_reminder():
     today = datetime.datetime.today()
+    # TODO filter by last_notification_date = today
     job_requests = JobRequest.objects.filter(Q(date_start__lte=today) & Q(date_end__gte=today) & Q(
         last_notification_status=constants.STATUS_15_MIN_SHIFT_START_ABS))
     for job_request in job_requests:
-        shift_end = datetime.datetime.combine(job_request.date_end, job_request.shift_time_end)
+        shift_end = datetime.datetime.combine(today, job_request.shift_time_end)
         if (datetime.datetime.now() - shift_end).total_seconds() >= 0:
             for assignment in job_request.assignments.all():
                 try:
@@ -88,11 +90,12 @@ def send_shifts_end_reminder():
 
 def send_shifts_end_15_min_ago_reminder():
     today = datetime.datetime.today()
+    # TODO filter by last_notification_date = today
     job_requests = JobRequest.objects.filter(Q(date_start__lte=today) & Q(date_end__gte=today) & Q(
         last_notification_status=constants.STATUS_REM_SHIFT_END))
     for job_request in job_requests:
-        shift_end = datetime.datetime.combine(job_request.date_end, job_request.shift_time_end)
-        if (shift_end - datetime.datetime.now()).total_seconds() >= 15:
+        shift_end = datetime.datetime.combine(today, job_request.shift_time_end)
+        if (datetime.datetime.now() - shift_end).total_seconds() >= 15:
             for assignment in job_request.assignments.all():
                 try:
                     shift_for_today = assignment.get_shift_for_date(today)
