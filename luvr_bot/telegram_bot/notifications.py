f7b40e01f25ec4b17f130587a6f04599b88d00b4
InaraShalfei <inarashugaikina@gmail.com>
2023-06-01 11:50:59 +0600

    Add try/except logic in notifications, fix filters and use model method in logic

--- luvr_bot/telegram_bot/notifications.py
+++ luvr_bot/telegram_bot/notifications.py
@@ -19,13 +19,19 @@ def send_shifts_start_soon_reminders():
     for job_request in job_requests:
         shift_start = datetime.datetime.combine(job_request.date_start, job_request.shift_time_start)
         if (shift_start - datetime.datetime.now()).total_seconds() <= 30 * 60:
-            job_request.last_notified_date = today
-            job_request.save()
             for assignment in job_request.assignments.all():
-                if assignment.employee.chat_id and assignment.shift.start_position is None:
-                    time_start_str = datetime.datetime.strftime(shift_start, '%H:%M')
-                    bot.send_message(chat_id=assignment.employee.chat_id,
-                                     text=f'Ваша смена начнется в {time_start_str}.\nНе забудьте отметиться')
+                try:
+                    shift_for_today = assignment.get_shift_for_date(today)
+                    if assignment.employee.chat_id and (shift_for_today is None or shift_for_today.start_position is None):
+                        time_start_str = datetime.datetime.strftime(shift_start, '%H:%M')
+                        # TODO add keybutton
+                        bot.send_message(chat_id=assignment.employee.chat_id,
+                                         text=f'Ваша смена начнется в {time_start_str}.\nНе забудьте отметиться')
+                except:
+                    pass
+        job_request.last_notified_date = today
+        job_request.last_notification_status = constants.STATUS_30_MIN_REM_SHIFT_START
+        job_request.save()
 
 
 def send_shifts_start_15_min_ago():
@@ -35,9 +41,16 @@ def send_shifts_start_15_min_ago():
         shift_start = datetime.datetime.combine(job_request.date_start, job_request.shift_time_start)
         if (datetime.datetime.now() - shift_start).total_seconds() >= 15 * 60:
             for assignment in job_request.assignments.all():
-                if assignment.shift.start_position is None and assignment.employee.chat_id:
-                    job_request.last_notified_date = today
-                    job_request.last_notification_status = constants.STATUS_15_MIN_SHIFT_START_ABS
-                    job_request.save()
-                    bot.send_message(chat_id=assignment.employee.chat_id,
-                                     text=f'Ваша смена началась 15 минут назад.\nНеобходимо отправить геоданные о Приходе')
+                try:
+                    shift_for_today = assignment.get_shift_for_date(today)
+                    if assignment.employee.chat_id and (shift_for_today is None or shift_for_today.start_position is None):
+
+                        # TODO add keybutton
+
+                        bot.send_message(chat_id=assignment.employee.chat_id,
+                                         text=f'Ваша смена началась 15 минут назад.\nНеобходимо отправить геоданные о Приходе')
+                except:
+                    pass
+        job_request.last_notified_date = today
+        job_request.last_notification_status = constants.STATUS_15_MIN_SHIFT_START_ABS
+        job_request.save()
