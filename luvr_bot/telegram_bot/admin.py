a5b1a8d84bd103aef79e3c1b11a1244dcc02aaec
InaraShalfei <inarashugaikina@gmail.com>
2023-06-01 14:40:26 +0600

    Add readable_broadcast field of JobRequest model in admin panel

--- luvr_bot/telegram_bot/admin.py
+++ luvr_bot/telegram_bot/admin.py
@@ -30,8 +30,8 @@ class BranchAdmin(admin.ModelAdmin):
 
 class JobRequestAdmin(admin.ModelAdmin):
     list_display = ('branch', 'employee_position', 'request_type', 'date_start', 'date_end', 'shift_time_start',
-                    'shift_time_end', 'number_of_employees', 'request_comment', 'employee', 'status', 'message_text',
-                    'request_date', 'last_notified_date', 'readable_notification_status')
+                    'shift_time_end', 'number_of_employees', 'request_comment', 'employee', 'status', 'request_date',
+                    'last_notified_date', 'readable_notification_status', 'readable_broadcast')
     inlines = [JobRequestAssignmentInline]
 
 
