a1f28ff95784e2e47111ff2ee764156c3d492277
inara <inarashugaikina@gmail.com>
2023-06-30 16:20:49 +0500

    Add new fields to Employee admin panel

--- luvr_bot/telegram_bot/admin.py
+++ luvr_bot/telegram_bot/admin.py
@@ -33,7 +33,7 @@ class BranchInline(admin.TabularInline):
 
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('phone_number', 'chat_id', 'INN', 'full_name', 'current_job_request', 'job_request_draft',
+    list_display = ('phone_number', 'language', 'jumis_go_user_id', 'chat_id', 'INN', 'full_name', 'current_job_request', 'job_request_draft',
                     'message_status')
     inlines = [JobRequestAssignmentInline]
     readonly_fields = ['chat_id']
